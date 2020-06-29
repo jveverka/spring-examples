ace7b94252aef75ff76d5494c7fd8a6a35f2c229
Juraj Veverka <gergej123@gmail.com>
2020-06-29 20:55:15 +0200

    docs improved

--- spring-proxy/README.md
+++ spring-proxy/README.md
@@ -4,7 +4,12 @@ This demo shows simple spring boot proxy server used to access other REST servic
 ![architecture](docs/architecture-proxy.svg)
 
 ### Build and run
+Provide your configuration file based on [this](src/main/resources/application.yml) example.
+Set ``proxy.target-url`` accordingly.
 ```
 gradle clean build test
-java -jar build/libs/spring-proxy-1.0.0-SNAPSHOT.jar
+java -jar build/libs/spring-proxy-1.0.0-SNAPSHOT.jar --spring.config.location=file:./src/main/resources/application.yml
 ```
+### Request Routing
+Provided ``proxy.target-url=http://localhost:8081/data``  
+__GET__ ``http://localhost:8088/proxy/info`` -> __GET__ ``http://localhost:8081/data/info``
--- spring-proxy/src/main/java/itx/examples/springboot/proxy/config/ProxyConfig.java
+++ spring-proxy/src/main/java/itx/examples/springboot/proxy/config/ProxyConfig.java
@@ -1,13 +1,19 @@
 package itx.examples.springboot.proxy.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @ConfigurationProperties(prefix = "proxy")
 public class ProxyConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProxyConfig.class);
 
     private String targetUrl;
 
@@ -17,6 +23,11 @@ public class ProxyConfig {
 
     public void setTargetUrl(String targetUrl) {
         this.targetUrl = targetUrl;
+    }
+
+    @PostConstruct
+    public void init() {
+        LOG.info("targetUrl={}", targetUrl);
     }
 
     @Bean
