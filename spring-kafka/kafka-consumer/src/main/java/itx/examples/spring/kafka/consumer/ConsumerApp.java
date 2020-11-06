1978fda5d0bd8c6205228c2cabc3857239e304c3
Juraj Veverka <gergej123@gmail.com>
2020-11-06 19:00:58 +0100

    bootstrapping kafka consumers and producers

---
+++ spring-kafka/kafka-consumer/src/main/java/itx/examples/spring/kafka/consumer/ConsumerApp.java
@@ -0,0 +1,13 @@
+package itx.examples.spring.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApp.class, args);
+    }
+
+}
---
+++ spring-kafka/kafka-producer/src/main/java/itx/examples/spring/kafka/producer/ProducerApp.java
@@ -0,0 +1,13 @@
+package itx.examples.spring.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApp.class, args);
+    }
+
+}
