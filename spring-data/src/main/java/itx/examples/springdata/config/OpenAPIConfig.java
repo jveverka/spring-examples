6e6161d58fb9620a1fac04c77471a1e9b5b54077
Juraj Veverka <gergej123@gmail.com>
2021-03-12 09:24:05 +0100

    adding swagger config

---
+++ spring-data/src/main/java/itx/examples/springdata/config/OpenAPIConfig.java
@@ -0,0 +1,28 @@
+package itx.examples.springdata.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Autowired(required = false) BuildProperties buildProperties) {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(new Info()
+                        .title("Spring-Data-Demo")
+                        .version((buildProperties != null) ? buildProperties.getVersion() : "SNAPSHOT")
+                        .description("")
+                        .license(new License().name("License: MIT")
+                                .url("https://opensource.org/licenses/MIT")));
+    }
+
+}
