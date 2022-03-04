f509b0350c71078a757017ecd3aa0a93fe60c722
Juraj Veverka <juraj.veverka@globallogic.com>
2022-03-04 16:32:28 +0100

    tests added

--- spring-data/src/test/java/itx/examples/springdata/test/DataAppTests.java
+++ spring-data/src/test/java/itx/examples/springdata/test/DataAppTests.java
@@ -15,6 +15,7 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,6 +24,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.lifecycle.Startables;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -96,6 +98,13 @@ class DataAppTests {
         ResponseEntity<UserData[]> responseEntity = restTemplate.getForEntity( "/services/users", UserData[].class);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals(0, responseEntity.getBody().length);
+    }
+
+    @Test
+    @Order(7)
+    void testOptions() {
+        Set<HttpMethod> httpMethods = restTemplate.optionsForAllow("/services/users");
+        assertEquals(4, httpMethods.size());
     }
 
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
